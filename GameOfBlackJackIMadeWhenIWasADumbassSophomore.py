db49c8bf8c427ab4d4a9aefe046c3bba663852db
ASalazar <anthonythesalazar028@gmail.com>
2025-09-12 19:50:22 -0700

    learn something

--- GameOfBlackJackIMadeWhenIWasADumbassSophomore.py
+++ GameOfBlackJackIMadeWhenIWasADumbassSophomore.py
@@ -1,3 +1,5 @@
+# If you're reading this, no you didn't. 
+
 import random
 
 cards =['Ace of Hearts', '2 of Hearts', '3 of Hearts', '4 of Hearts', '5 of Hearts', '6 of Hearts', '7 of Hearts', '8 of Hearts', '9 of Hearts', '10 of Hearts', 'Jack of Hearts', 'Queen of Hearts', 'King of Hearts',
